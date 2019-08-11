c6b4a4842fa28cd38285f3c2911982e2d59671df
corona <rlaehdgkr7@naver.com>
2019-08-11 23:10:43 +0900

    input example

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -2,12 +2,27 @@
 using namespace std;
 int main() {
 	int t;
+	while (true) {
+		std::cin >> t;
+		std::cout << "입력:: " << t << std::endl;
+		if (std::cin.fail())
+		{
+			std::cout << "제대로 다시 입력해주세요. " << std::endl;
+			std::cin.clear();
+			std::cin.ignore(100, '\n');
+		}
+		if (t == 0)break;
+	}
+}
+
+/*int main() {
+	int t;
 	while (std::cin >> t) {
 		std::cout << "input:: " << t << std::endl;
 		if (t == 0) break;
 	}
 }
-/*int main() {
+int main() {
 	string a;
 //	std::cout << "hello\n";
 	int t;
