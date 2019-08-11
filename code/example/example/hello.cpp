114a009cb3f7f56f2214d9c27b41a69ba275c781
corona <rlaehdgkr7@naver.com>
2019-08-11 23:26:08 +0900

    sum of two variables

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,8 +1,11 @@
 ﻿#include<iostream>
 using namespace std;
 int main() {
-	int a;
+	int a,b,total;
 	std::cout << "Please input data :: ";
 	std::cin >> a;
-	std::cout << "This is input data :: " << a << std::endl;
+	std::cout << "Please input second data :: ";
+	std::cin >> b;
+	total = a + b;
+	std::cout << "This is sum data :: " << a << " + " << b << " = " <<total << std::endl;
 }
