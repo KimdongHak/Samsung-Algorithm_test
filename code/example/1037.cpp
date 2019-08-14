caec1ae65fbe247b9b19f73cf6f709aa289109d8
corona <rlaehdgkr7@naver.com>
2019-08-14 22:10:29 +0900

    update 1037.cpp

--- code/example/1037.cpp
+++ code/example/1037.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 void main()
 {
-	int n;
-	int a, b;
-	std::cin >> n;
-	std::cout << n << endl;
+	int N=1, n;
+	int a[]; // N의 진짜 약수
+	std::cin >> n; //N의 약수의 개수
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i]; // N의 진짜 약수 입력
+		N = a[i]*N;
+	}
+	
+	std::cout << N << endl;
 }
