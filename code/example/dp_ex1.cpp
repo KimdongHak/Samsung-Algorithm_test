4b52f91c09994bcb796b57c788ee219ffffac8f0
BCNLab <rlaehdgkr7@naver.com>
2019-08-25 13:30:55 +0900

    add dp_ex1.cpp

--- code/example/dp_ex1.cpp
+++ code/example/dp_ex1.cpp
@@ -1,6 +1,6 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
-int cache[30][30] = { -1,};
+int cache[30][30] = { -1, };
 int solution(int n, int r)
 {
 	if (r == 0 | n == r) return 1;
@@ -9,13 +9,13 @@ int solution(int n, int r)
 	return cache[n][r] = solution(n - 1, r - 1) + solution(n - 1, r);
 }
 int main() {
-	int n,r;
+	int n, r;
 	std::cin >> n;
 	std::cin >> r;
-	solution(n,r);
+	solution(n, r);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < r; j++) {
-			cout << cache[i][j] ;
+			cout << cache[i][j];
 		}
 	}
 }
