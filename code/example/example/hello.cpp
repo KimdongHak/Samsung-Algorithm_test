b61684d6d2cb601dd619f7b4bad5fd4c8293e585
corona92 <rlaehdgkr7@naver.com>
2020-01-06 21:18:39 +0900

    add 10828.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,38 +1,76 @@
 ﻿#include<iostream>
 #include<string>
-#include<cstring>
+#include<algorithm>
 using namespace std;
-string convert(string s) {
-	int cnt = 0;
-	string str_s;
-	string stack[100]; //stack 배열
-	for (int i = 0; i < s.size()+1; i++) {
-		stack[cnt] = s[i];
-		if (s[i] == ' ' || s[i] == '\0') {
-			for (int i = cnt-1; i >= 0; i--) {
-				str_s = str_s + stack[i];
-			}
-			cnt = 0; // 스택값 초기화
-			str_s = str_s + ' '; // 공백 추가
+int cnt=0;
+int result[50] = { 0, };
+int buf[50] = { 0, };
+int stack(string s) {
+	int push_value = 0, pop_value = 0, size=0;
+	//pop 기능
+	if (s.find("pop") != string::npos) {
+		if (cnt <= 0) {
+			cout << "stack이 없습니다." << endl;
+			cnt = -1;
+			return cnt;
 		}
 		else {
-			cnt += 1; // 스택값 증가
+			pop_value = buf[0];
+
+			//stack 값 이동
+			for (int i = 0; i < cnt; i++) {
+				buf[i] = buf[i + 1];
+			}
+			cnt -= 1;
+		}
+//		cout << "remain stack :: " << endl;
+		for (int i = 0; i < cnt; i++) {
+//			cout << "index " <<i << " "<<buf[i] << endl;
+		}
+		return pop_value;
+	}
+	// top 출력
+	if (s.find("top") != string::npos) {
+		if (cnt == -1) {
+			return cnt;
+		}
+		return buf[0];
+	}
+	//size 출력
+	if (s.find("size") != string::npos) {
+		return cnt;
+	}
+
+	// push 기능
+	if (s.find("push") != string::npos) {
+		push_value = atoi(s.substr(5).c_str());
+		for (int i = cnt; i > 0; i--) {
+			buf[i] = buf[i-1];
+		}
+		buf[0] = push_value;
+		cnt += 1;
+		return push_value;
+	}
+	if (s.find("empty") != string::npos) {
+		if (cnt <= 0) {
+			return 1;
+		}
+		else {
+			return 0;
 		}
 	}
-	return str_s;
 }
 int main() {
 	int t = 0; // testcase
+
 	string str;
-	string print[100]; //testcase에 따른 print 횟수
 	cin >> t;
 	cin.ignore();
 	for (int i = 0; i < t; i++) {
 		getline(cin, str);
-		print[i] = convert(str); 
+		result[i]=stack(str);
 	}
 	for (int i = 0; i < t; i++) {
-		cout << print[i] << '\n';
+		cout << result[i] << endl;
 	}
-	
 }
