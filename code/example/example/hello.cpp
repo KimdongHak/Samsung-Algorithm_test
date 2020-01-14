cefb01494d61d11da19c62fbb421be8b41a54e19
corona <rlaehdgkr7@naver.com>
2020-01-14 19:23:33 +0900

    add 2581.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,44 +1,47 @@
-﻿// 1. 처음에 공간을 만든다
-// 2. joisino가 숫자 하나를 방송한다. 만약 그 숫자가 종이위에 씌어진 숫자라면 종이에서 숫자를 지운다.
-// 만약 아니라면, 종이위에 적는다. 이 과정은 N번 반복한다.
-// 3. 그러고 나서, 질문을 물어본다. 현재 숫자가 얼마나 시트에 적혀져 있는가?
-// 게임이 끝날때 까지 몇개의 숫자가 적혀져 있겠는가?
-#include<iostream>
-#include<vector>
-
-#include<ctime>
+﻿#include<iostream>
 using namespace std;
-int ans[100001];
+string d[36][36][36];
+
+void ans(int num) {
+	d[1][0][0] = "*";
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num; j++) {
+			cout << d[i][j] << endl;
+		}
+
+	}
+}
 int main() {
-	ios_base::sync_with_stdio(false);
-	clock_t start = clock();
-	int testcase;
+	int testcase,N, posX=1, posY, posZ=1;
 	cin >> testcase;
-	for (int i = 0; i < testcase; i++) {
-		long long N, nnn;
-		int cnt=0;
+	d[0][0][0] = "*";
+	for (int i = 1; i <= testcase; i++) {
 		cin >> N;
-		vector<int> buf(N);
-		for (int j = 0; j < N; j++) {
-			cin >> nnn;
-			for (int j = 0; j < buf.size(); j++) {
-				if (buf[j] == nnn) {
-					buf.erase(buf.begin() + j);
-					cnt -= 1;
-					break;
-				}
-				if (j == buf.size() - 1) {
-					buf.push_back(nnn);
-					cnt++;
-					break;
+		// n % 3 = 1 이면 아래에 복붙
+		if (N % 3 == 1) {
+			for (int x = 1; x < 36; x++) {
+				for (int y= 1; y < 36; y++) {
+					d[x][y][posZ] = d[x][y][posZ - 1];
+					d[x*2][y*2][posZ] = d[x][y][posZ-1];
 				}
 			}
 		}
-		ans[i] = cnt;
-	}
-	for(int i=1;i<=testcase;i++)
-		cout << "#" << i << " " << ans[i-1] << endl;
 
-	cout << "걸린시간 :: "<< (float)(clock() - start) / CLOCKS_PER_SEC << endl;
+		// n % 3 = 2 이면 오른쪽에 별 찍기
+		else if (N % 3 == 2) {
+			
+		}
+
+		// n % 3 = 0 이면 맨 아랫줄에 별 쫘르륵 찍기
+		else if (N % 3 == 0) {
+
+		}
+	}
+
+
+	for (int i = 1; i <= testcase; i++) {
+		cout << "#" << i << endl;
+		ans(i);
+	}
 	return 0;
 }
