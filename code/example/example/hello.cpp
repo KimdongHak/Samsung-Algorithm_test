4f4f53d55601ec9cb43829ad8980c581cc4bb3cb
corona <rlaehdgkr7@naver.com>
2020-01-18 14:46:40 +0900

    practice iter

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,44 +1,22 @@
-﻿#include <string>
-#include <vector>
-#include<iostream>
-
+﻿#include<iostream>
+#include<vector>
 using namespace std;
+int main()
+{
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
 
-vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<string> answer;
-    string buf = "";
-    int* binary = new int[n];
-    int indx = 0, i = 0;
-    while (true) {
-        binary[indx] = arr1[i] % 2 + arr2[i] % 2;
-        arr1[i] = arr1[i] / 2;
-        arr2[i] = arr2[i] / 2;
-        if (binary[indx] == 2 || binary[indx] == 1)
-            buf = buf + "#";
-        else {
-            buf = buf + " ";
-        }
-        if (arr1[i] == 0) {
-            answer.push_back(buf);
-            buf = "";
-            i += 1;
-            indx = 0;
-        }
-        indx += 1;
-    }
-    return answer;
-}
-int main() {
-    
-    int n = 0;
-    vector<int> arr1;
-    vector<int> arr2;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr1[i];
-    }
-    for (int i = 0; i < n; i++) {
-        cin >> arr2[i];
-    }
-    cout << solution(n, arr1, arr2) << endl;
+	vector<int>::iterator iter;
+
+	for (iter = v.begin(); iter != v.end(); ++iter) {
+		//cout << *iter << endl;
+		if (iter = find(v.begin(), v.end(), 20))
+			cout << *iter << endl;
+	}
+
+	return 0;
 }
