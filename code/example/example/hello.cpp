39f0e9ececd9bbd433543331fac950edf99c7e32
corona <rlaehdgkr7@naver.com>
2020-01-19 00:01:45 +0900

    update hello.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,22 +1,27 @@
-﻿#include<iostream>
-#include<vector>
+﻿#include <iostream>
 using namespace std;
+long long d[1001][10];
+long long mod = 10007;
 int main() {
-	vector <int> v;
-	v.push_back(10);
-	v.push_back(20);
-	v.push_back(30);
-	v.push_back(40);
-	v.push_back(50);
+    int n;
+    cin >> n;
 
-	vector<int>::iterator normal_iter = v.begin() + 3;
-
-	//v.begin()의 index = 0, +3이면 40을 가리킴
-	
-	vector<int>::reverse_iterator reverse_iter(normal_iter);
-
-	// 맨 뒤 Null값 제외하고 그 앞을 가리키는 것을 기준으로 index 3이면 30
-
-	cout << "normal_iter:: " << *normal_iter << endl;
-	cout << "reverse_iter:: " << *reverse_iter << endl;
+    for (int i = 0; i <= 9; i++) {
+        d[1][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k <= j; k++) {
+                d[i][j] += d[i - 1][k];
+                d[i][j] %= mod;
+            }
+        }
+    }
+    long long ans = 0;
+    for (int i = 0; i < 10; i++) {
+        ans += d[n][i];
+    }
+    ans %= mod;
+    cout << ans << '\n';
+    return 0;
 }
