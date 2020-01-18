a24f5830d8cdc374d7afe4975475a21c9aa0bba5
corona <rlaehdgkr7@naver.com>
2020-01-18 16:30:21 +0900

    study about iterator in c++_1

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,22 +1,22 @@
 ﻿#include<iostream>
 #include<vector>
 using namespace std;
-int main()
-{
-	vector<int> v;
+int main() {
+	vector <int> v;
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
 	v.push_back(40);
 	v.push_back(50);
 
-	vector<int>::iterator iter;
+	vector<int>::iterator normal_iter = v.begin() + 3;
 
-	for (iter = v.begin(); iter != v.end(); ++iter) {
-		//cout << *iter << endl;
-		if (iter = find(v.begin(), v.end(), 20))
-			cout << *iter << endl;
-	}
+	//v.begin()의 index = 0, +3이면 40을 가리킴
+	
+	vector<int>::reverse_iterator reverse_iter(normal_iter);
 
-	return 0;
+	// 맨 뒤 Null값 제외하고 그 앞을 가리키는 것을 기준으로 index 3이면 30
+
+	cout << "normal_iter:: " << *normal_iter << endl;
+	cout << "reverse_iter:: " << *reverse_iter << endl;
 }
