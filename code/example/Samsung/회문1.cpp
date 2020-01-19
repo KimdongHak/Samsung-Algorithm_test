21dd72d3226f0d2b7c731db6947ece9977022580
corona <rlaehdgkr7@naver.com>
2020-01-19 16:40:56 +0900

    update directory and add 회문1.cpp

---
+++ code/example/Samsung/회문1.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+char board[8][8] = { 0, };
+int main() {
+	// 10번의 테스트케이스
+//	for (int i = 0; i < 10; i++) {
+	int sen_len = 0; //찾아야하는 회문의 길이
+	cin >> sen_len;
+	string buf = "";
+	int count = 0, answer = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cin >> board[i][j];
+		}
+	}
+	// 가로
+	for (int i = 0; i < 8; i++) {
+		buf = "";
+		for (int j = 0; j < 8; j++) {
+			buf += board[i][j];
+		}
+		// 회문 가로 검사
+		for (int a = 0; a < 8 - sen_len; a++) {
+			for (int b = 0; b < sen_len; b++) {
+				if (buf[b] == buf[sen_len - b]) {
+					count++;
+					if (count == sen_len / 2) {
+						answer += 1;
+						count = 0;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	//세로
+	for (int i = 0; i < 8; i++) {
+		buf = "";
+		for (int j = 0; j < 8; j++) {
+			buf += board[j][i];
+		}
+		//회문 세로 검사
+		for (int a = 0; a < 8 - sen_len; a++) {
+			for (int b = 0; b < sen_len; b++) {
+				if (buf[b] == buf[sen_len - b]) {
+					count++;
+					if (count == sen_len / 2) {
+						answer += 1;
+						count = 0;
+						break;
+					}
+				}
+			}
+		}
+	}
+	cout << answer << endl;
+	//	}
+}
--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,27 +1,61 @@
-﻿#include <iostream>
+﻿#include<iostream>
+#include<string>
 using namespace std;
-long long d[1001][10];
-long long mod = 10007;
-int main() {
-    int n;
-    cin >> n;
 
-    for (int i = 0; i <= 9; i++) {
-        d[1][i] = 1;
-    }
-    for (int i = 2; i <= n; i++) {
-        for (int j = 0; j <= 9; j++) {
-            for (int k = 0; k <= j; k++) {
-                d[i][j] += d[i - 1][k];
-                d[i][j] %= mod;
-            }
-        }
-    }
-    long long ans = 0;
-    for (int i = 0; i < 10; i++) {
-        ans += d[n][i];
-    }
-    ans %= mod;
-    cout << ans << '\n';
-    return 0;
+char board[8][8] = { 0, };
+int main() {
+	// 10번의 테스트케이스
+//	for (int i = 0; i < 10; i++) {
+		int sen_len = 0; //찾아야하는 회문의 길이
+		cin >> sen_len;
+		string buf = "";
+		int count = 0, answer = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				cin >> board[i][j];
+			}
+		}
+		// 가로
+		for (int i = 0; i < 8; i++) {
+			buf = "";
+			for (int j = 0; j < 8; j++) {
+				buf += board[i][j];
+			}
+			// 회문 가로 검사
+			for (int a = 0; a < 8 - sen_len; a++) {
+				for (int b = 0; b < sen_len; b++) {
+					if (buf[b] == buf[sen_len - b]) {
+						count++;
+						if (count == sen_len / 2) {
+							answer += 1;
+							count = 0;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		//세로
+		for (int i = 0; i < 8; i++) {
+			buf = "";
+			for (int j = 0; j < 8; j++) {
+				buf += board[j][i];
+			}
+			//회문 세로 검사
+			for (int a = 0; a < 8 - sen_len; a++) {
+				for (int b = 0; b < sen_len; b++) {
+					if (buf[b] == buf[sen_len - b]) {
+						count++;
+						if (count == sen_len / 2) {
+							answer += 1;
+							count = 0;
+							break;
+						}
+					}
+				}
+			}
+		}
+		cout << answer << endl;
+//	}
 }
