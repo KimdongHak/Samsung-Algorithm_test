24026410bef520ef24bf589eeb07e45b35e14679
corona <rlaehdgkr7@naver.com>
2020-03-12 16:11:42 +0900

    try to solve 6603-lotto.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -5,18 +5,28 @@ using namespace std;
 int main() {
     int k;
     vector <int> casetotal;
+    
+
     do {
         cin >> k;
         vector <int> s(k);
+        vector <int> ind(k); //Vector of index value
         for (int i = 0; i < k; i++) {
             cin >> s[i];
+            ind[i] = 1;
+        }
+        for (int i = 0; i < k - ind.size(); i++) {
+            ind.push_back(0);
         }
         do {
             // 순열 조합
             for (int i = 0; i < k; i++) {
-                cout << s[i] << " ";
+                if (ind[i] == 1) {
+                    cout << s[i] << " ";
+                }
             }
             cout << '\n';
+
         } while (next_permutation(s.begin(), s.end()));
 
     } while (k != 0);
