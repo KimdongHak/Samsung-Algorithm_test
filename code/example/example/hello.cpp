b5551995f3f9cc1ccc5b649ef3b85b7fd28b53ff
corona <rlaehdgkr7@naver.com>
2020-03-13 00:42:07 +0900

    add 1759.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -1,31 +1,33 @@
-﻿#include <iostream>
+﻿// 암호만들기_1759.cpp
+#include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
-int go(int sum, int goal) {
-    if (sum > goal) {
-        return 0;
-    }
-    if (sum == goal) {
-        return 1;
-    }
-    int now = 0;
-    for (int i = 1; i <= 3; i++) {
-        now += go(sum + i, goal);
-    }
-    return now;
+
+int l, c;
+
+void answer(int idx, vector<char> &buf,int cnt, int mo, int ja, string ans) {
+	if (cnt == l) {
+		if (mo >= 1 && ja >= 2)
+			cout << ans << '\n';
+		return;
+	}
+	if (idx == c)
+		return;
+	if (buf[idx] == 'a' || buf[idx] == 'e' || buf[idx] == 'i' || buf[idx] == 'o' || buf[idx] == 'u')
+		answer(idx + 1, buf, cnt + 1, mo + 1, ja, ans + buf[idx]);
+	else
+		answer(idx + 1, buf, cnt + 1, mo, ja + 1, ans + buf[idx]);
+	answer(idx + 1, buf, cnt, mo, ja, ans);
 }
+
 int main() {
-    int t, i=0;
-    cin >> t;
-    vector <int> result(t);
-    while (t--) {
-        int n;
-        cin >> n;
-        result[i] = go(0, n);
-        i++;
-    }
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i] << '\n';
-    }
-    return 0;
+	cin >> l >> c;
+	vector <char> buf(c);
+	for (int i = 0; i < c; i++) 
+		cin >> buf[i];
+	
+	sort(buf.begin(), buf.end());
+	answer(0, buf, 0, 0, 0, "");
+	return 0;
 }
