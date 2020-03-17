a75b931edcca231dd904ffe1b3a47ebdd3abccea
corona <rlaehdgkr7@naver.com>
2020-03-17 14:09:21 +0900

    add 15649.cpp

---
+++ code/example/Codeplus_c++/SW역량평가/N과M(1)_15649.cpp
@@ -0,0 +1,31 @@
+//15649_N°úM(1).cpp
+#include<iostream>
+#include<vector>
+using namespace std;
+bool check[10];
+int a[10];
+
+void solve(int idx, int n, int m) {
+	if (idx == m) {
+		for (int i = 0; i < m; i++) {
+			cout << a[i];
+			if (i != m - 1) cout << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (check[i]) continue;
+		check[i] = true;
+		a[idx] = i;
+		solve(idx + 1, n, m);
+		check[i] = false;
+	}
+}
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	solve(0, N, M);
+	return 0;
+}
