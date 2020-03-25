80124100af1c628c786822806662c9a008fa0dc5
corona <rlaehdgkr7@naver.com>
2020-03-25 22:47:27 +0900

    add 2667.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -2,5 +2,20 @@
 #include<vector>
 using namespace std;
 int main() {
+	int n;
+	cin >> n;
+	vector <vector<int>> matrix(n, vector<int> (n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
 	return 0;
 }
