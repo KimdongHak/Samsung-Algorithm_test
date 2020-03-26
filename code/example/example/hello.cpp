f9dded812e8b7983264f1c5281c28bc8b13111fe
corona <rlaehdgkr7@naver.com>
2020-03-26 20:42:53 +0900

    add hyundai_ngv coding test

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -2,16 +2,19 @@
 #include<vector>
 using namespace std;
 int main() {
-	int t;
-	cin >> t;
-	vector<int> result(t);
-	for (int i = 0; i < t; i++) {
+	int n, m;
+	cin >> n >> m;
+	vector <int> v(n);
+	// 노드 입력
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	// 질문
+	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
-		result[i] = a + b;
 	}
-	for (int i = 0; i < t; i++) {
-		cout << "Case #" << i+1 << ": " << result[i] << '\n';
-	}
+
+	
 	return 0;
 }
