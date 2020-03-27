bc2d8df0c53ea21d5c13364897c77629a7d1ed53
corona <rlaehdgkr7@naver.com>
2020-03-27 23:17:31 +0900

    update hello.cpp

--- code/example/example/hello.cpp
+++ code/example/example/hello.cpp
@@ -2,19 +2,6 @@
 #include<vector>
 using namespace std;
 int main() {
-	int n, m;
-	cin >> n >> m;
-	vector <int> v(n);
-	// 노드 입력
-	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-	}
-	// 질문
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-	}
 
-	
 	return 0;
 }
